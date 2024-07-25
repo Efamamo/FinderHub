0eb1d44f05c6c3eb16e6373b4ea69bb96f6def39
ephrem <ephremmamo555@gmail.com>
2024-07-25 11:57:05 +0300

    title and favicon changed

--- finderhub/src/componentes/Home/Home.jsx
+++ finderhub/src/componentes/Home/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
             Welcome to FinderHub - your go-to platform for finding people across
             the top social media networks. Whether reconnecting with old
             friends, networking with professionals, or exploring new
-            connections, FinderHu makes it easy to search for users on Facebook,
+            connections, FinderHub makes it easy to search for users on Facebook,
             Twitter, Instagram, LinkedIn, Snapchat, TikTok, Reddit,
             Pinterest,Youtube and GitHub. Start your search today and discover
             the convenience of FinderHub!
